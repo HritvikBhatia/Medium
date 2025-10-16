75b2a8e023e1919652be8042d4b174e07fed1258
vik <hritvik111@gmail.com>
2025-10-16 17:56:41 +0530

    fix: appbar on signin and signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { Blog } from './pages/Blog'
@@ -8,20 +7,31 @@ import { Blogs } from './pages/Blogs'
 import { Publish } from './pages/Publish'
 import { Appbar } from './components/Appbar'
 
-function App() {
+function AppLayout() {
   return (
     <>
-      <BrowserRouter>
-          <Appbar/>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
+      <Appbar />
+      <Outlet /> {/* Renders child routes */}
+    </>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Routes without Appbar */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+
+        {/* Routes with Appbar */}
+        <Route element={<AppLayout />}>
           <Route path="/blog/:id" element={<Blog />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/publish" element={<Publish />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
